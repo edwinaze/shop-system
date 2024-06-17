f3b8d398e0f38db4d74df2cc2550944a36531865
Aze <lynuACM@outlook.com>
2024-06-17 16:32:15 +0800

    实现购物车功能以及购物车页面优化

--- src/components/goods/MyTable.vue
+++ src/components/goods/MyTable.vue
@@ -1,27 +1,73 @@
 <template>
-    <table class="table table-striped table-bordered">
-      <!-- 表格标题区域 -->
-      <thead>
-        <tr>
-          <slot name="header"></slot>
-        </tr>
-      </thead>
-      <!-- 表格主体区域 -->
-      <tbody>
-        <tr v-for="(item, index) in goodsList" :key="item.id">
-          <slot name="body" :row="item" :index="index"></slot>
-        </tr>
-      </tbody>
-    </table>
-  </template>
-   
-  <script setup>
-  const props = defineProps({
-    goodsList: {
-      type: Array,
-      required: true,
-      default: []
-    }
-  })
-  </script>
-  
+	<table
+		class="table table-striped table-bordered"
+		style="table-layout: fixed; word-break: break-all"
+	>
+		<!-- 表格标题区域 -->
+		<thead>
+			<tr>
+				<slot name="header"></slot>
+			</tr>
+		</thead>
+		<!-- 表格主体区域 -->
+		<tbody>
+			<tr v-for="(item, index) in goodsList" :key="item.id">
+				<slot name="body" :row="item" :index="index"></slot>
+			</tr>
+			<tr>
+				<td>
+					<input
+						class="form-check-input"
+						type="checkbox"
+						id="checkAll"
+						:value="amount"
+						@click="checkAll"
+					/>
+					<label class="form-check-label ms-2" for="checkAll">
+						全选
+					</label>
+				</td>
+				<td colspan="5">合计：￥{{ totalPrice }}</td>
+			</tr>
+		</tbody>
+	</table>
+</template>
+
+<script setup>
+import { ref, watch } from "vue";
+
+const totalPrice = ref(0);
+const isCheckAll = ref(false);
+
+watch(
+	() => props.goodsList.map((item) => item.amount),
+	(newVal, oldVal) => {
+		console.log(newVal, oldVal);
+		updatePrice();
+	},
+	{ deep: true }
+);
+
+const updatePrice = () => {
+	totalPrice.value = 0;
+	props.goodsList.forEach((item) => {
+		totalPrice.value += item.amount * item.goods_price;
+	});
+	console.log(totalPrice.value);
+};
+
+const checkAll = (e) => {
+	isCheckAll.value = e.target.checked;
+	props.goodsList.forEach((item) => {
+		item.checkState = isCheckAll.value;
+	});
+};
+
+const props = defineProps({
+	goodsList: {
+		type: Array,
+		required: true,
+		default: [],
+	},
+});
+</script>
--- src/components/subcomponents/MyGoods.vue
+++ src/components/subcomponents/MyGoods.vue
@@ -1,107 +1,174 @@
 <template>
-  <h4>商品管理</h4>
-  
-  <MyTable :goodsList="list">
-    <template v-slot:header>
-      <th scope="col">#</th>
-      <th scope="col">商品名称</th>
-      <th scope="col">价格</th>
-      <th scope="col">标签</th>
-      <th scope="col">操作</th>
-    </template>
-    <!-- row-list中的每一项，index-下标 -->
-    <template #body="{ row, index }">
-      <td>
-        <div class="view">
-          <input class="toggle" type="checkbox" v-model="row.checkState" />
-        </div>
-      
-      </td>
-      <td>{{ index + 1 }}</td>
-      <td>{{ row.goods_name }}</td>
-      <td>￥{{ row.goods_price }}</td>
-      <td>
-        <input type="text" v-if="row.inputVisible" class="form-control form-control-sm ipt-tag" v-focus
-          v-model="row.inputValue" 
-          @keyup.esc="row.inputValue = ''"
-          @blur="onInputConfig(row)"
-          @keyup.enter="onInputConfig(row)"
-        />
-        <button class="btn btn-outline-primary rounded-pill" v-else @click="row.inputVisible = true">+Tag</button>
-        <span class="btn btn-outline-dark" v-for="item in row.tags" :key="item">
-          {{ item }}
-        </span>
-      </td>
-      <td>
-        <button type="button" class="btn btn-outline-danger btn-sm" @click="onRemove(row.id)">删除</button>
-      </td>
-    </template>
+	<h4>商品管理</h4>
 
-  </MyTable>
-  <GoodsFooter :lastLength="list.length"  @updateStatus="updateStatus" />
-  
+	<MyTable :goodsList="list">
+		<template v-slot:header>
+			<th scope="col" style="width: 90px">#</th>
+			<th scope="col" style="width: 50px">序号</th>
+			<th scope="col">商品名称</th>
+			<th scope="col" style="width: 100px">价格</th>
+			<th scope="col" style="width: 200px">数量</th>
+			<th scope="col">标签</th>
+			<th scope="col" style="width: 100px">操作</th>
+		</template>
+		<!-- row-list中的每一项，index-下标 -->
+		<template #body="{ row, index }">
+			<td>
+				<div class="view">
+					<input
+						class="form-check-input"
+						type="checkbox"
+						v-model="row.checkState"
+					/>
+				</div>
+			</td>
+			<td>{{ index + 1 }}</td>
+			<td>{{ row.goods_name }}</td>
+			<td>￥{{ row.goods_price }}</td>
+			<td class="flex-container">
+				<!-- <div class="row gy-2 gx-3 align-items-center">
+					<div class="col-auto"> -->
+				<button
+					type="button"
+					class="mr-0 btn btn-light rounded-circle"
+					@click="changeAmount(row, -1)"
+				>
+					-
+				</button>
+				<!-- </div>
 
+					<div class="col-sm-2"> -->
+				<input
+					type="tel"
+					class="form-control"
+					:value="row.amount"
+					style="width: 50px"
+				/>
+				<!-- </div>
+					<div class="col-auto"> -->
+				<button
+					type="button"
+					class="btn btn-primary rounded-circle"
+					@click="changeAmount(row, 1)"
+				>
+					+
+				</button>
+				<!-- </div>
+				</div> -->
+			</td>
+			<td>
+				<input
+					type="text"
+					v-if="row.inputVisible"
+					class="form-control form-control-sm ipt-tag"
+					v-focus
+					v-model="row.inputValue"
+					@keyup.esc="row.inputValue = ''"
+					@blur="onInputConfig(row)"
+					@keyup.enter="onInputConfig(row)"
+				/>
+				<button
+					class="btn btn-outline-primary rounded-pill"
+					v-else
+					@click="row.inputVisible = true"
+				>
+					+Tag
+				</button>
+				<span
+					class="btn btn-outline-dark"
+					v-for="item in row.tags"
+					:key="item"
+				>
+					{{ item }}
+				</span>
+			</td>
+			<td>
+				<button
+					type="button"
+					class="btn btn-outline-danger btn-sm"
+					@click="onRemove(row.id)"
+				>
+					删除
+				</button>
+			</td>
+		</template>
+	</MyTable>
+	<!-- <GoodsFooter :lastLength="list.length" @updateStatus="updateStatus" /> -->
 </template>
- 
+
 <script setup>
-import MyTable from '../goods/MyTable.vue'
-import GoodsFooter from '../goods/GoodsFooter.vue'
-import { ref, computed } from 'vue'
-import request from '../../axios/request'
-const list = ref([])
-request.get('mock/getGoodsList').then(response=>{
-console.log(response)
-list.value=response.data.data[0].list
-}).catch(error=>{
-  console.log(error)
-})
+import MyTable from "../goods/MyTable.vue";
+import GoodsFooter from "../goods/GoodsFooter.vue";
+import { ref, computed } from "vue";
+import request from "../../axios/request";
+const list = ref([]);
+request
+	.get("mock/getGoodsList")
+	.then((response) => {
+		console.log(response);
+		list.value = response.data.data[0].list;
+	})
+	.catch((error) => {
+		console.log(error);
+	});
 
+const changeAmount = (row, num) => {
+	if (row.amount + num < 0) return;
+	row.amount += num;
+};
 
-const onRemove = id => {
-  list.value = list.value.filter(item => item.id != id)
-}
-const vFocus = el => { el.focus() }
+const onRemove = (id) => {
+	list.value = list.value.filter((item) => item.id != id);
+};
+const vFocus = (el) => {
+	el.focus();
+};
 
-const onInputConfig = row => {
-  const val = row.inputValue
-  row.inputValue = ''
-  row.inputVisible = false
-  if (!val || row.tags.indexOf(val) !== -1) {
-    return
-  }
-	row.tags.push(val)
-}
-
-
+const onInputConfig = (row) => {
+	const val = row.inputValue;
+	row.inputValue = "";
+	row.inputVisible = false;
+	if (!val || row.tags.indexOf(val) !== -1) {
+		return;
+	}
+	row.tags.push(val);
+};
 
 const updateStatus = (status) => {
-  if (status === 'all') {
-    list.value.forEach(item => {
-      item.checkState=true
-    });
-    return list.value;
-  }
-  if (status === 'active') {
-    return list.value=list.value.filter(item => !item.checkState)
-  }
-  if (status === 'completed') {
-    return list.value=[]
-  }
-}
+	if (status === "all") {
+		list.value.forEach((item) => {
+			item.checkState = true;
+		});
+		return list.value;
+	}
+	if (status === "active") {
+		return (list.value = list.value.filter((item) => !item.checkState));
+	}
+	if (status === "completed") {
+		return (list.value = []);
+	}
+};
 </script>
 
 <style scoped>
+.flex-container {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
 th {
-  text-align: center;
+	text-align: center;
 }
 td {
-  line-height: 30px;
+	line-height: 30px;
 }
 .ipt-tag {
-  width: 80px;
-  display: inline;
+	width: 80px;
+	display: inline;
 }
-input, span, button {
-  margin-right: 10px;
+input,
+span,
+button {
+	margin-right: 10px;
 }
 </style>
